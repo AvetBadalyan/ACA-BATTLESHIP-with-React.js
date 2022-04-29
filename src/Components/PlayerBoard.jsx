e21eb14b4f14ea5e82a0b325a3890102dcdc488a
AvetBadalyan <avetbadalyan@gmail.com>
2022-04-30 00:14:07 +0400

    feat: adding some styles to hit the ships

--- src/Components/PlayerBoard.jsx
+++ src/Components/PlayerBoard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./../App.css";
 import groupArray from "../helpers/groupArray";
-import setClass from "../helpers/setClass";
+import setClass, { setPlayerClass } from "../helpers/setClass";
 import { ACTION_TYPES } from "../State/State";
 
 export default function PlayerBoard({ state, dispatch, player }) {
@@ -15,22 +15,22 @@ export default function PlayerBoard({ state, dispatch, player }) {
     }
   };
 
-      const hitShip = (id) => {
-       if ( state.player1.ships.has(id)) {
-           dispatch({ type: ACTION_TYPES.SET_BEATEN, id });
-       } else {
-            dispatch({ type: ACTION_TYPES.SET_PASS, id });
-       }
-      };
-    
+  const hitShip = (id) => {
+    if (state.player1.ships.has(id)) {
+      dispatch({ type: ACTION_TYPES.SET_BEATEN, id });
+    } else {
+      dispatch({ type: ACTION_TYPES.SET_PASS, id });
+    }
+  };
+
   return (
-      <div className={setClass(isSetShipMode, "set-ships-mode")}>
-          <h1> Player {player} </h1>
+    <div className={setClass(isSetShipMode, "set-ships-mode")}>
+      <h1> Player {player} </h1>
       {groupArray().map((row) => (
         <div className="row">
           {row.map((square) => (
             <div className="square" onClick={() => setShips(square)}>
-              <div className={setClass(ships.has(square), "has-ship")}></div>
+              <div className={setPlayerClass(state.player1, square)}></div>
             </div>
           ))}
         </div>
--- src/helpers/setClass.js
+++ src/helpers/setClass.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
-export default function setClass(condition, class1, class2="") {
+const CLASS_TYPES = {
+  SHIP: "has-ship",
+  PASS: "pass",
+  HIT: "hit",
+};
+
+export default function setClass(condition, class1, class2 = "") {
   if (condition) {
-      return class1;
+    return class1;
   } else {
-      return class2;
+    return class2;
   }
 }
+
+export const setPlayerClass = (state, id) => {
+  let classes = [];
+  if (state.ships.has(id)) {
+    classes.push(CLASS_TYPES.SHIP);
+  }
+  if (state.beaten.has(id)) {
+    classes.push(CLASS_TYPES.HIT);
+  }
+  if (state.pass.has(id)) {
+    classes.push(CLASS_TYPES.PASS);
+  }
+  return classes.join(" ");
+};
