95d32de79914578cf5a1c5ee5f3de992feb65615
AvetBadalyan <avetbadalyan@gmail.com>
2022-06-14 20:41:57 +0400

    feat: adding a choose the player page

--- src/Pages/ChoosePlayerPage.jsx
+++ src/Pages/ChoosePlayerPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./../App.css";
+import { Link } from 'react-router-dom';
 
 export default function ChoosePlayerPage() {
   return (
-    <div>ChoosePlayerPage</div>
-  )
+    <div className="choose-player-page">
+      <h1>CHOOSE THE PLAYER</h1>
+      <div className="choose-player-buttons">
+        <Link to={"/players/player1"}>
+          <button className="player-button">Player 1</button>
+        </Link>
+
+        <Link to={"/players/player2"}>
+          <button className="player-button">Player 2</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
